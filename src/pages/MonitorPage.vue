377b78562485388a53f799ef0c226e752931ea33
unknown <greimerabelguzman29@gmail.com>
2024-02-19 07:38:08 -0400

    implementando firebase en el proyecto

--- src/pages/MonitorPage.vue
+++ src/pages/MonitorPage.vue
@@ -1,173 +1,251 @@
 <template>
   <q-card @changeState="onMounted()">
-        <q-tabs
-          v-model="tab"
-          dense
-          class="text-grey"
-          active-color="primary"
-          indicator-color="primary"
-          narrow-indicator
+    <q-tabs
+      v-model="tab"
+      dense
+      class="text-grey"
+      active-color="primary"
+      indicator-color="primary"
+      narrow-indicator
+    >
+      <q-tab name="history" label="historial" />
+      <q-tab name="students" label="Estudiantes" />
+      <q-tab name="soli" label="solicitudes" />
+    </q-tabs>
+
+    <q-separator />
+
+    <q-tab-panels v-model="tab" animated>
+      <q-tab-panel name="history">
+        <q-table
+          title="Historial de permisos"
+          :rows="row"
+          :columns="column"
+          :rows-per-page-options="[0]"
+          virtual-scroll
+          no-data-label="No se encontraron registros"
+          no-results-label="No se encontraron coincidencias"
         >
-          <q-tab name="history" label="historial" />
-          <q-tab name="students" label="Estudiantes" />
-          <q-tab name="soli" label="solicitudes" />
-        </q-tabs>
+          <template v-slot:top-right>
+            <q-btn
+              label="Export"
+              color="primary"
+              icon-right="archive"
+              no-caps
+              @click="exportTable"
+            />
+          </template>
+        </q-table>
+      </q-tab-panel>
 
-        <q-separator />
-
-        <q-tab-panels v-model="tab" animated>
-          <q-tab-panel name="history">
-            <q-table
-              title="Historial de permisos"
-              :rows="row"
-              :columns="column"
-              :rows-per-page-options="[0]"
-              virtual-scroll
-              no-data-label="No se encontraron registros"
-              no-results-label="No se encontraron coincidencias"
+      <q-tab-panel name="students">
+        <q-card>
+          <div class="q-mb-md">
+            <div
+              v-show="students.length == 0"
+              class="flex flex-center text-bold text-italic"
             >
-              <template v-slot:top-right>
-                <q-btn label="Export" color="primary" icon-right="archive" no-caps @click="exportTable"/>
-              </template>
-            </q-table>
-          </q-tab-panel>
-
-          <q-tab-panel name="students">
-            <q-card>
-              <div class="q-mb-md">
-                <div v-show="students.length == 0" class="flex flex-center text-bold text-italic">
-                  <p class="text-grey-7">...No tienes alumnos registrados</p>
-                </div>
-              </div>
-
-              <BadgeComponent style="border-radius: 5px;" :students="students"  />
-            </q-card>
-          </q-tab-panel>
-
-          <q-tab-panel name="soli" class="q-px-xs" >
-            <div v-show="pendigSoli == 0" class="flex flex-center text-bold text-italic">
-              <p  class="text-grey-7">...No tiene solicitudes pendientes</p>
+              <p class="text-grey-7">...No tienes alumnos registrados</p>
             </div>
-            <SoliComponent :soliPending="pendigSoli" @RecallPermises="getSoliPermises"/>
-          </q-tab-panel>
-        </q-tab-panels>
-      </q-card>
+          </div>
+
+          <BadgeComponent style="border-radius: 5px" :students="students" />
+        </q-card>
+      </q-tab-panel>
+
+      <q-tab-panel name="soli" class="q-px-xs">
+        <div
+          v-show="pendigSoli == 0"
+          class="flex flex-center text-bold text-italic"
+        >
+          <p class="text-grey-7">...No tiene solicitudes pendientes</p>
+        </div>
+        <SoliComponent
+          :soliPending="pendigSoli"
+          @RecallPermises="getSoliPermises"
+        />
+      </q-tab-panel>
+    </q-tab-panels>
+  </q-card>
 </template>
 
 <script>
-import { defineComponent,ref, onMounted } from 'vue'
-import { api } from 'src/boot/axios';
-import {date } from 'quasar';
-import exportXlsFile from 'export-from-json'
-import HistoryComponent from 'src/components/HistoryComponent.vue';
-import SoliComponent from 'src/components/SoliComponent.vue';
-import BadgeComponent from 'src/components/BadgeComponent.vue';
+import { defineComponent, ref, onMounted } from "vue";
+import { api } from "src/boot/axios";
+import { date } from "quasar";
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import exportXlsFile from "export-from-json";
+import HistoryComponent from "src/components/HistoryComponent.vue";
+import SoliComponent from "src/components/SoliComponent.vue";
+import BadgeComponent from "src/components/BadgeComponent.vue";
 
 export default defineComponent({
-  name: 'MonitorPage',
-  components:{
+  name: "MonitorPage",
+  components: {
     BadgeComponent,
     HistoryComponent,
-    SoliComponent
+    SoliComponent,
   },
-  setup(){
-    const pendigSoli = ref([])
-    const tableData = ref(['nombre','apellido'])
-    const students = ref([])
-    const userPermise = ref([])
-    const row = ref([])
+  setup() {
 
-    setInterval(()=>{
-      getStudents()
-      getSoliPermises()
-      getHistoryPermises()
-    },2000)
-    onMounted(()=>{
-      getSoliPermises()
-      getHistoryPermises()
-    })
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyASaQkUftrTTDp_5PfoQvBoH6SFJUlsgsM",
+      authDomain: "homeplusnotify-f6088.firebaseapp.com",
+      projectId: "homeplusnotify-f6088",
+      storageBucket: "homeplusnotify-f6088.appspot.com",
+      messagingSenderId: "566040100897",
+      appId: "1:566040100897:web:0df17c3c685cd2884c6360",
+      measurementId: "G-ZE97SL1PEE"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // Get registration token. Initially this makes a network call, once retrieved
+    // subsequent calls to getToken will return from cache.
+    const messaging = getMessaging();
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      // ...
+    });
+
+    getToken(messaging, { vapidKey: 'BCdquCm3NTt7JvX3zxKFkNGiDQ8ijctXSU3BK3Ke9pTuaALdH34BR-kjxBvVh5NcPSk-z8tMXOAXM6VSSitDYE4' })
+    .then((currentToken) => {
+      if (currentToken) {
+        // Send the token to your server and update the UI if necessary
+        console.log('token is:', currentToken)
+        // ...
+      } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      // ...
+    });
+
+    const pendigSoli = ref([]);
+    const tableData = ref(["nombre", "apellido"]);
+    const students = ref([]);
+    const userPermise = ref([]);
+    const row = ref([]);
+
+    setInterval(() => {
+      getStudents();
+      getSoliPermises();
+      getHistoryPermises();
+    }, 2000);
+    onMounted(() => {
+      getSoliPermises();
+      getHistoryPermises();
+    });
     const getStudents = () => {
-      api.get('users/students')
-        .then(res => {
-          students.value = res.data
-        })
-    }
+      api.get("users/students").then((res) => {
+        students.value = res.data;
+      });
+    };
 
     const getSoliPermises = () => {
-      api.get('users/permises/requests')
-        .then(res => {
-          pendigSoli.value = res.data
-          pendigSoli.value.forEach(element => {
-            element.fecha_salida = date.formatDate(element.fecha_salida, 'DD-MM-YYYY')
-            element.fecha_llegada = date.formatDate(element.fecha_llegada, 'DD-MM-YYYY')
-          });
-        })
-    }
+      api.get("users/permises/requests").then((res) => {
+        pendigSoli.value = res.data;
+        pendigSoli.value.forEach((element) => {
+          element.fecha_salida = date.formatDate(
+            element.fecha_salida,
+            "DD-MM-YYYY"
+          );
+          element.fecha_llegada = date.formatDate(
+            element.fecha_llegada,
+            "DD-MM-YYYY"
+          );
+        });
+      });
+    };
 
     const getHistoryPermises = () => {
-      api.get('users/permises')
-        .then(res => {
-          tableData.value = res.data
-          tableData.value.forEach(element => {
-            element.fecha_salida = date.formatDate(element.fecha_salida, 'DD-MM-YYYY')
-            element.fecha_llegada = date.formatDate(element.fecha_llegada, 'DD-MM-YYYY')
-            element.hora_llegada = `${element.users.nombre} ${element.users.apellido}`
-          })
-          row.value = tableData.value
-        })
-    }
+      api.get("users/permises").then((res) => {
+        tableData.value = res.data;
+        tableData.value.forEach((element) => {
+          element.fecha_salida = date.formatDate(
+            element.fecha_salida,
+            "DD-MM-YYYY"
+          );
+          element.fecha_llegada = date.formatDate(
+            element.fecha_llegada,
+            "DD-MM-YYYY"
+          );
+          element.hora_llegada = `${element.users.nombre} ${element.users.apellido}`;
+        });
+        row.value = tableData.value;
+      });
+    };
 
     const column = [
-      {name:'alumName',label:'Estudiante',field:'hora_llegada'},
-      {name:'alumFsalida',label:'Fecha de Salida',field:'fecha_salida'},
-      {name:'alumFLlegada',label:'Fecha de Llegada',field:'fecha_llegada'},
-      {name:'alumHSalida',label:'Hora de Salida',field:'hora_salida_firmada'},
-      {name:'alumHLlegada',label:'Hora de Llegada',field:'hora_llegada_firmada'},
-      {name:'alumTipoPermiso',label:'tipo de permiso',field:'tipo'},
-      {name:'alumUsedPermise',label:'lugar',field:'lugar'},
-      {name:'alumEstadoPermiso',label:'Estado',field:'estado'},
-      {name:'alumUsedPermise',label:'Usado',field:'usado'},
-    ]
+      { name: "alumName", label: "Estudiante", field: "hora_llegada" },
+      { name: "alumFsalida", label: "Fecha de Salida", field: "fecha_salida" },
+      {
+        name: "alumFLlegada",
+        label: "Fecha de Llegada",
+        field: "fecha_llegada",
+      },
+      {
+        name: "alumHSalida",
+        label: "Hora de Salida",
+        field: "hora_salida_firmada",
+      },
+      {
+        name: "alumHLlegada",
+        label: "Hora de Llegada",
+        field: "hora_llegada_firmada",
+      },
+      { name: "alumTipoPermiso", label: "tipo de permiso", field: "tipo" },
+      { name: "alumUsedPermise", label: "lugar", field: "lugar" },
+      { name: "alumEstadoPermiso", label: "Estado", field: "estado" },
+      { name: "alumUsedPermise", label: "Usado", field: "usado" },
+    ];
 
     const exportTable = () => {
-
-      tableData.value.forEach(element => {
+      tableData.value.forEach((element) => {
         userPermise.value.push({
-          'Estudiante':`${element.users.nombre} ${element.users.apellido}`,
-          'Fecha de salida':element.fecha_salida,
-          'Fecha de llegada':element.fecha_llegada,
-          'Hora de salida':element.hora_salida_firmada,
-          'Hora de llegada':element.hora_llegada_firmada,
-          'Tipo de permiso':element.tipo,
-          'Lugar':element.lugar,
-          'Estado':element.estado,
-          'Usado':element.usado,
-        })
-      })
-      const fileName = 'Tabla_Permisos'
-      const exportType = exportXlsFile.types.xls //json, css, html, xml, txt
-      exportXlsFile({data:userPermise.value,fileName, exportType})
-    }
+          Estudiante: `${element.users.nombre} ${element.users.apellido}`,
+          "Fecha de salida": element.fecha_salida,
+          "Fecha de llegada": element.fecha_llegada,
+          "Hora de salida": element.hora_salida_firmada,
+          "Hora de llegada": element.hora_llegada_firmada,
+          "Tipo de permiso": element.tipo,
+          Lugar: element.lugar,
+          Estado: element.estado,
+          Usado: element.usado,
+        });
+      });
+      const fileName = "Tabla_Permisos";
+      const exportType = exportXlsFile.types.xls; //json, css, html, xml, txt
+      exportXlsFile({ data: userPermise.value, fileName, exportType });
+    };
 
-    return{
-      tab: ref('soli'),
+    return {
+      tab: ref("soli"),
       row,
       column,
       students,
       pendigSoli,
       exportTable,
-      getSoliPermises
-    }
-  }
-})
+      getSoliPermises,
+    };
+  },
+});
 </script>
 <style>
-.q-banner__actions{
+.q-banner__actions {
   justify-content: start;
   justify-content: left;
 }
 p {
-margin: 0;
-justify-content: left;
+  margin: 0;
+  justify-content: left;
 }
 </style>
