5893cf4f9d051cbcea0f4d3a133cee6e165ebc9f
unknown <greimerabelguzman29@gmail.com>
2024-02-19 21:44:50 -0400

    working notify

--- src/pages/MonitorPage.vue
+++ src/pages/MonitorPage.vue
@@ -88,8 +88,6 @@ export default defineComponent({
     SoliComponent,
   },
   setup() {
-
-
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     const firebaseConfig = {
